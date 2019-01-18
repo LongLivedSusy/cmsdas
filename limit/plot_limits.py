f9893206796ccfeed25b2434b6a439d22dd36e98
caleb-james-smith <caleb_smith2@baylor.edu>
2019-01-17 20:16:54 -0600

    change function to accept json file

--- limit/plot_limits.py
+++ limit/plot_limits.py
@@ -12,15 +12,29 @@ gStyle.SetOptStat(0)
 TH1D.SetDefaultSumw2()
 
 
-def plotLimit(limits):
-    n_bins = len(limits)
-    h_limit = TH1D("h_limit","h_limit", n_bins, 0, n_bins)
-    for i, limit in enumerate(limits):
-        h_limit.Fill(i, limit) 
+def plotLimit(json_file):
+    with open(json_file, "r") as read_file:
+        data = json.load(read_file)
+    
+    ctau_list = [float(ct) for ct in data]
+    ctau_list.sort()
+    
+    for ctau in ctau_list:
+        print "{0} : {1}".format(ctau, data[str(ctau)]["exp0"])
+    
+    exp0_list = [data[str(ct)]["exp0"] for ct in ctau_list]
+    
+    print ctau_list
+    print exp0_list
+    n_bins = len(exp0_list)
+    h_exp0 = TH1D("h_exp0","h_exp0", n_bins, 0, n_bins)
+    for i, limit in enumerate(exp0_list):
+        h_exp0.Fill(i, limit) 
     canvas = TCanvas("canvas", "canvas", 900, 800)
-    h_limit.Draw("hist E")
-    h_limit.GetYaxis().SetRangeUser(0, 0.2)
-    h_limit.SetTitle("Limits")
+    h_exp0.Draw("hist")
+    h_exp0.GetYaxis().SetRangeUser(0, 0.2)
+    h_exp0.SetTitle("Limits")
+    h_exp0.SetLineColor(kBlack)
     canvas.SaveAs("limits.pdf")
 
 # # Physics processes
@@ -39,18 +53,5 @@ def plotLimit(limits):
 
 json_file = "limits.json"
 
-with open(json_file, "r") as read_file:
-    data = json.load(read_file)
-
-ctau_list = [float(ct) for ct in data]
-ctau_list.sort()
-
-for ctau in ctau_list:
-    print "{0} : {1}".format(ctau, data[str(ctau)]["exp0"])
-
-exp0_list = [data[str(ct)]["exp0"] for ct in ctau_list]
-
-print ctau_list
-print exp0_list
-plotLimit(exp0_list)
+plotLimit(json_file)
 
